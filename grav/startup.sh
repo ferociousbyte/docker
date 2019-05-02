abb1d561d30de066c22bb3fa9cdb71156324e129
xkodiak <xkodiak@talarionstudios.com>
2019-05-03 01:35:01 +0200

    fix permission problems

--- grav/startup.sh
+++ grav/startup.sh
@@ -3,8 +3,15 @@
 BASE="/var/www/html"
 DEFAULT="$BASE/user_default"
 USER="$BASE/user"
+BACKUP="$BASE/backup"
 
-test "$(ls -A $USER)" || (cp -R $DEFAULT/* $USER && chown www-data:www-data -R $USER)
+test "$(ls -A $USER)" || cp -R $DEFAULT/* $USER
+
+chown www-data:www-data -R $USER
+chown www-data:www-data -R $BACKUP
+chmod 755 -R $USER
+chmod 755 -R $BACKUP
+
 rm -R $DEFAULT
 
 exec "$@"
