04cb104e12b4a42436bea739730c0591f2f0bc2c
xkodiak <xkodiak@talarionstudios.com>
2019-05-03 00:47:04 +0200

    grav> exec "$@" and shorten Dockerfile

--- grav/startup.sh
+++ grav/startup.sh
@@ -4,6 +4,7 @@ BASE="/var/www/html"
 DEFAULT="$BASE/user_default"
 USER="$BASE/user"
 
-test "$(ls -A $USER)" || cp -R "$DEFAULT/*" "$USER"
-rm $DEFAULT
-apache2_foreground
+test "$(ls -A $USER)" || (cp -R $DEFAULT/* $USER && chown www-data:www-data -R $USER)
+rm -R $DEFAULT
+
+exec "$@"
