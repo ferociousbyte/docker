afb024c9f51ed55d9f8195267dacbd0f69c646c0
xkodiak <xkodiak@talarionstudios.com>
2019-05-05 19:16:14 +0200

    fix

--- grav/startup.sh
+++ grav/startup.sh
@@ -5,13 +5,26 @@ DEFAULT="$BASE/user_default"
 USER="$BASE/user"
 BACKUP="$BASE/backup"
 
-test "$(ls -A $USER)" || cp -R $DEFAULT/* $USER
+if [ "$(ls -A $USER)" ]; then
+    printf "Content in $USER already exists ... skip\n"
+else
+    printf "Copy initial data to $USER ... "
+    cp -R $DEFAULT/* $USER
+    printf "done\n"
+fi
 
-chown www-data:www-data -R $USER
-chown www-data:www-data -R $BACKUP
-chmod 755 -R $USER
-chmod 755 -R $BACKUP
+printf "Remove directory $DEFAULT ... "
+test -d $DEFAULT && rm -R $DEFAULT
+printf "done\n"
 
-rm -R $DEFAULT
+printf "Fix permissions of $BASE ... "
+chown -R www-data:www-data $BASE
+find $BASE -type f | xargs chmod 664
+find $BASE/bin -type f | xargs chmod 775
+find $BASE -type d | xargs chmod 775
+find $BASE -type d | xargs chmod +s
+umask 0002
+printf "done\n"
 
+printf "Run $@\n"
 exec "$@"
